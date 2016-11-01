bf1795aabe019339a0e03f1645138818d3b2017f
Damiaan Dufaux <damiaan@dpro.be>
2016-11-01 01:17:14 +0100

    Initial Commit

---
+++ Sort Lines/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sort Lines
+//
+//  Created by Damiaan on 01-11-16.
+//  Copyright © 2016 Damiaan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
